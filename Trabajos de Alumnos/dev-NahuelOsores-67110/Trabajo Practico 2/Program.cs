96d7e04cfb7597f1636a423f5261e6bca75cb935
SilverShadow571 <nosores9@gmail.com>
2026-09-19 19:47:27 -0300

    Menu
    Menu con opcion de ejercicios

--- Trabajos de Alumnos/dev-NahuelOsores-67110/Trabajo Practico 2/Program.cs
+++ Trabajos de Alumnos/dev-NahuelOsores-67110/Trabajo Practico 2/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("==========Ejercicios==========");
+            Console.WriteLine();
+            Console.WriteLine("1. Sistema de inventario y Durabilidad.");
+            Console.WriteLine("2. Marcador TOP 5 de Speedrun.");
+            Console.WriteLine("3. Combate contra hordas de slimes.");
+            Console.WriteLine("4. Canje de recompensas en Tienda de cartas.");
+            Console.WriteLine("5. Registro y Filtrado de daño por rafaga.");
+            Console.WriteLine("6. Selector de Elementos y Cargas Magicas.");
+            Console.WriteLine("7. Deteccion de trampas en pasillo de Mazmorra.");
+            Console.WriteLine("8. Estadisticas de fases del Boss.");
+            Console.WriteLine("9. Sistema de municion y Disparo por cargador.");
+            Console.WriteLine("10. Asignador y Verificador de bonificacion de EXP.");
+            Console.WriteLine("11. Salir");
+            Console.WriteLine();
+            Console.WriteLine("==============================");
         }
     }
 }
