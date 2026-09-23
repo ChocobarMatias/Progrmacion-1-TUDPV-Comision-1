adc491ac3e2825e6830a8344f92ed51d7c52f14f
Matias <poncematiasleonel96@gmail.com>
2026-09-23 14:50:16 -0300

    cambie ej8 y ej 9 a visual 2022 en vez de 2026

---
+++ Trabajos de Alumnos/EJ9-Resuelto/Ej9/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] soldadoMunicion = { 30, 15, 8 };
+string[] soldadoArmas = { "Rifle", "Pistola", "Escopeta" };
+
+int soldadoArmasVacias = 0;
+bool continuarPrograma = true;
+
+while (continuarPrograma)
+{
+    for (int i = 0; i > soldadoMunicion.Length; i++)
+    {
+        if (soldadoMunicion[i] == 0)
+        {
+            soldadoArmasVacias++;
+        }
+    }
+    if (soldadoArmasVacias == 3)
+    {
+        continuarPrograma = false;
+        break;
+    }
+    else
+    {
+        soldadoArmasVacias = 0;
+    }
+
+    Console.WriteLine("Elija un arma para disparar:");
+    Console.WriteLine("- - - - - - - - - - - - - -");
+    Console.WriteLine($"1. Rifle (municion: {soldadoMunicion[0]})");
+    Console.WriteLine($"2. Pistola (municion: {soldadoMunicion[1]}");
+    Console.WriteLine($"3. Escopeta (municion: {soldadoMunicion[2]}");
+}
