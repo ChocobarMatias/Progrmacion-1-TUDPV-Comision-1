17a9f015e1fe98f54d7223a135489a708b9cfa5f
cyphertrip1 <sofialuzr23@gmail.com>
2026-09-23 23:52:42 -0300

    Ejercicio1

---
+++ Trabajos de Alumnos/67150-SofiaLuzRodriguezVilla/Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,30 @@
+﻿float[] tiempos = new float[5];
+float[] tiempoObjetivo = new float[1];
+
+for (int i = 0; i < tiempos.Length; i++)
+{
+   
+      Console.WriteLine("Ingrese el tiempo del runner " + (i + 1));
+      tiempos[i] = float.Parse(Console.ReadLine());
+
+}
+
+while (tiempoObjetivo[0] <= 0)
+{
+    Console.WriteLine("Definir tiempo objetivo");
+
+    tiempoObjetivo[0] = float.Parse(Console.ReadLine());
+
+}
+
+for(int i = 0; i < tiempos.Length; i++)
+{
+    if (tiempos[i] <= tiempoObjetivo[0])
+    {
+        Console.WriteLine("El runner " + (i + 1) + " llegó al objetivo");
+    }
+    else
+    {
+        Console.WriteLine("El runner " + (i + 1) + " no llegó al objetivo");
+    }
+}
