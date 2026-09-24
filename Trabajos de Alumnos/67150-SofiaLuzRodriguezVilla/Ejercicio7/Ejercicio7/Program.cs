8b5c0fc90ce1661e9fc1c43ab65013074dedbb46
cyphertrip1 <sofialuzr23@gmail.com>
2026-09-24 03:25:29 -0300

    Ejercicio7

---
+++ Trabajos de Alumnos/67150-SofiaLuzRodriguezVilla/Ejercicio7/Ejercicio7/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Ejercicio 7: Detección de Trampas en Pasillo de Mazmorra*/
+
+bool[] trampas = {false, true, false, false, true, false};
+bool meta = false;
+bool caida = false;
+
+
+while (!meta && !caida)
+{
+    for (int i = 0; i < trampas.Length; i++)
+    {
+        if (trampas[i] == true)
+        {
+            caida = true;
+            Console.WriteLine("¡Caíste en la trampa!");
+            break;
+        }
+
+        else
+        {
+            if (trampas[i] == false)
+            {
+                Console.WriteLine("¡Baldosa segura!");
+            }
+            
+        }
+    }
+
+    if (!caida)
+    {
+        Console.WriteLine("¡Llegaste a la meta!");
+        meta = true;
+    }
+}
